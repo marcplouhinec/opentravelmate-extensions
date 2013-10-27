e198c0e43f54b849bce5d1f3f8ba0c14276935cf
Marc Plouhinec <m_plouhinec@yahoo.fr>
2013-10-27 14:54:19 +0100

    Remove debug information.

--- itinerary-finder/itineraryFinder.js
+++ itinerary-finder/itineraryFinder.js
@@ -13,10 +13,9 @@ define([
     '../core/widget/map/LatLng',
     '../core/widget/map/Marker',
     '../core/widget/map/UrlMarkerIcon',
-    '../core/widget/map/SvgPathMarkerIcon',
     '../services4otm-publictransport/Services4otmPlaceProvider',
     './itineraryPanel'
-], function($, Widget, webview, Point, Dimension, LatLng, Marker, UrlMarkerIcon, SvgPathMarkerIcon, Services4otmPlaceProvider, itineraryPanel) {
+], function($, Widget, webview, Point, Dimension, LatLng, Marker, UrlMarkerIcon, Services4otmPlaceProvider, itineraryPanel) {
     'use strict';
 
     var itineraryFinder = {
@@ -154,26 +153,6 @@ define([
         '_showItineraries': function(itineraries) {
             // Show the first itinerary only
             itineraryPanel.open(itineraries[0]);
-
-            // DEBUG ONLY !!!
-            var map  = /** @Type {Map} */ Widget.findById('map');
-            var marker = new Marker({
-                position: new LatLng(49.61, 6.131),
-                title: 'DEBUG ONLY',
-                icon: new SvgPathMarkerIcon({
-                    anchor: new Point(0, 0),
-                    size: new Dimension(40, 40),
-                    fillColor: 'yellow',
-                    fillOpacity: 0.8,
-                    path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-                    rotation: 0,
-                    scale: 1,
-                    strokeColor: 'gold',
-                    strokeOpacity: 1,
-                    strokeWeight: 14
-                })
-            });
-            map.addMarkers([marker]);
         }
     };
 
