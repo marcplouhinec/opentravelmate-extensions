5ad5880ca4c9c30151cb6ea578ac90b941195bac
Marc Plouhinec <m_plouhinec@yahoo.fr>
2014-09-20 16:48:01 +0200

    Add date criterion in the itinerary finder.

--- itinerary-finder/itinerary-finder-subwebview/internalController.js
+++ itinerary-finder/itinerary-finder-subwebview/internalController.js
@@ -4,11 +4,13 @@
  * @author marc.plouhinec@gmail.com (Marc Plouhinec)
  */
 
+define('modernizr', [], window.Modernizr);
 define([
+    'modernizr',
     'googleFastButton',
     '../../core/widget/webview/webview',
     './constants'
-], function(FastButton, webview, constants) {
+], function(modernizr, FastButton, webview, constants) {
     'use strict';
 
     var internalController = {
@@ -16,7 +18,23 @@ define([
          * Initialize the SubWebView.
          */
         'initWebView': function() {
-            // TODO
+            // Initialize the date and time fields
+            $('#itineraryDate').val(moment().format('YYYY-MM-DD'));
+            $('#itineraryTime').val(moment().format('HH:mm'));
+
+            // Activate the date picker
+            if (!modernizr.inputtypes.date) {
+                $('input[type=date]').datepicker({
+                    dateFormat: 'yy-mm-dd'
+                });
+            }
+            if (!modernizr.inputtypes.time) {
+                $('input[type=time]').timepicker({
+                    timeFormat: 'H:i',
+                    step: 10
+                });
+            }
+
 
             // Fire an external event when the close button is pressed
             new FastButton(document.getElementById('close-button'), function() {
